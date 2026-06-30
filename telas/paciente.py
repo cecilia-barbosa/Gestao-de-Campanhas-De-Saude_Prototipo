d71621f341b6752f5edee2bf337cb09aaf17b029
ju-barbosa <jubarbosa2211@gmail.com>
2026-06-30 22:27:29 +0000

    listar e inserir

--- telas/paciente.py
+++ telas/paciente.py
@@ -11,3 +11,14 @@ pn.extension()
 engine = create_engine(
     "postgresql+psycopg2://postgres:0808@localhost:5432/trabalho_FBD"
 )
+
+#funcao listar
+def listar_pacientes():
+    with engine.connect() as conn:
+        resultado = conn.execute(text("SELECT * FROM Paciente ORDER BY cns"))
+        colunas = list(resultado.keys())
+        dados = resultado.fetchall()
+    df = pd.DataFrame(dados, columns=colunas)
+    if not df.empty:
+        df['data_de_nascimento'] = pd.to_datetime(df['data_de_nascimento']).dt.date
+    return df
